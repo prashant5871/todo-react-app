6baa72f5580608afdc2f3bc87ed2dca261c55e1d
Prashant Kalsariya <chhabilbhaikalsariya@gmail.com>
2024-04-11 17:42:53 +0530

    show/hide completed task is completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import { v4 as uuidv4 } from 'uuid';
 function App() {
   const [todo, setTodo] = useState("")
   const [todos, setTodos] = useState([])
+  const [showComplete, setshowComplete] = useState(false)
+
+  // useEffect(() => {
+  //   let temp = showComplete;
+  //   setshowComplete(!temp);
+  // }, [showComplete])
 
   useEffect(() => {
     // setTodos(JSON.parse(localStorage.getItem('myTodo')))
@@ -13,10 +19,14 @@ function App() {
     console.log("data is  : " + data)
     if (data) {
       setTodos(JSON.parse(data));
-      console.log("all todos are : " ,JSON.parse(data))
+      console.log("all todos are : ", JSON.parse(data))
     }
-  },[])
+  }, [])
 
+
+  const handleShowTodo = ()=>{
+    setshowComplete(!showComplete)
+  }
 
   const storeToLocalStorage = (t) => {
     let myTodos = [...todos]
@@ -108,7 +118,7 @@ function App() {
 
 
           {/* Option for showing completed tasks */}
-          {todos.length != 0 && <div className="showOption flex gap-3 mt-1 mx-3 text-lg items-center">
+          {todos.length != 0 && <div onChange={handleShowTodo} className="showOption flex gap-3 mt-1 mx-3 text-lg items-center">
             <input type="checkbox" name="" id='condition' className='cursor-pointer' />
             <label htmlFor="condition" className='cursor-pointer'>Show completed tasks</label>
           </div>}
@@ -117,16 +127,18 @@ function App() {
           {/* All todo will be wrap in inside this container which is .todos */}
           <div className="todos">
             {todos.map((item, index) => {
-              return <div key={item.id} className="todo flex justify-between my-3 p-3 bg-green-300 w-full">
-                <div className="left flex gap-3 text-lg items-center justify-center">
-                  <input onChange={(e) => { handleConditionChange(e, item.id) }} type="checkbox" checked={item.isComplete} />
-                  <div className={item.isComplete ? "line-through" : ""}><p>{item.todo}</p></div>
+              if ((showComplete == true && item.isComplete == true) || (item.isComplete == false)) {
+                return <div key={item.id} className="todo flex justify-between my-3 p-3 bg-green-300 w-full">
+                  <div onClick={(e) => { handleConditionChange(e, item.id) }} className="cursor-pointer left flex gap-3 text-lg items-center justify-center">
+                    <input onChange={(e) => { handleConditionChange(e, item.id) }} type="checkbox" checked={item.isComplete} />
+                    <div className={item.isComplete ? "line-through" : ""}><p>{item.todo}</p></div>
+                  </div>
+                  <div className="right flex gap-4 justify-center items-center">
+                    <button className='border rounded-md p-2 bg-green-800 text-white hover:bg-green-700' onClick={(event) => { handleUpdate(event, item.id) }}>Update</button>
+                    <button onClick={(event) => { handleDelete(event, item.id) }} className='border rounded-md p-2 bg-red-800 text-white hover:bg-red-700'>delete</button>
+                  </div>
                 </div>
-                <div className="right flex gap-4">
-                  <button className='border rounded-lg p-2 bg-green-800 text-white hover:bg-green-700' onClick={(event) => { handleUpdate(event, item.id) }}>Update</button>
-                  <button onClick={(event) => { handleDelete(event, item.id) }} className='border rounded-lg p-2 bg-red-800 text-white hover:bg-red-700'>delete</button>
-                </div>
-              </div>
+              }
             })}
 
             {/* <div className="todo flex justify-between my-3 p-3 bg-green-300 w-full">
